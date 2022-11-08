14fd0fffb9ce8184798195ca2e07c8b32a8edb1e
sandeepTw <sandeep@thoughtwin.com>
2022-11-08 17:46:49 +0530

    fix-model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+
+class Role(models.Model):  
+    name = models.CharField(max_length=50)
+    permission = models.TextField(max_length=150)  
+
+class User(AbstractUser):
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    
+    
+class BaseModel(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    updated_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, blank=True)
+    created_on=models.DateTimeField(auto_now_add=True)
+    updated_on=models.DateTimeField(auto_now=True)   
+    
+    class Meta:
+        abstract = True
+
+class Project(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+      
+   
--- inhouse_console/settings.py
+++ inhouse_console/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
@@ -83,7 +85,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
+AUTH_USER_MODEl = 'account.User'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
